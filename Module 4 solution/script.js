6ee1545569087e365b455ac042621efeb2857434
SUJAL LUHAR <98218454+sujal-luhar@users.noreply.github.com>
2023-01-18 18:12:16 +0530

    Update script.js

--- Module 4 solution/script.js
+++ Module 4 solution/script.js
@@ -13,7 +13,7 @@ names[9]="jim";
 
 for (var i = 0; i < names.length; i++) {
 	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
-        console.log("Goodbye "+ names[i])
+        console.log("Good bye "+ names[i])
 	}
 	else{
 		console.log("Hello "+ names[i])
